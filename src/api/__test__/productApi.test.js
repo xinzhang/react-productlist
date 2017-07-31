81a2f3a8a9d052f4b3062386cbe4ad53b431951d
Xin Zhang <Xin.Zhang@ausmaq.com.au>
2017-07-31 17:13:22 +1000

    change to global.fetch

--- src/api/__test__/productApi.test.js
+++ src/api/__test__/productApi.test.js
@@ -3,11 +3,25 @@ import {getAllProducts} from '../productApi';
 describe("Api", function () {
 
   beforeEach(function() {
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({ok: true, Id: '123'}));
+    global.fetch = jest.fn().mockImplementation(() => {
+      var p = new Promise((resolve, reject) => {
+       resolve({
+         ok: true,
+         Id: '123',
+         json: function() {
+           return {Id: '123'}
+         }
+       });
+     });
+
+     return p;
+
+    });
   });
 
   it("ack", function () {
-
+    //const response = await getAllProducts();
+    //console.log(response);
     getAllProducts().then(response => {
       console.log(response); //< this is never printed
       expect(response.Id).toBe(1); //< always pass / gets ignored?
